f21d4b965257672b84a7226aea9bf3a8ff16ef1d
daniarkin <jishikukuto@gmail.com>
2022-01-31 21:54:56 +0300

    graphics by text

--- js/tetris.js
+++ js/tetris.js
@@ -1,6 +1,32 @@
-'use strict';
+// 'use strict';
 
-const brick = [{ x: 3, y: 3 }, { x: 4, y: 3 }, { x: 5, y: 3 }, { x: 4, y: 4 }];
+// const brick = [{ x: 3, y: 3 }, { x: 4, y: 3 }, { x: 5, y: 3 }, { x: 4, y: 4 }];
 const block = document.querySelectorAll('.block');
 
-brick.forEach(coords => block[coords.y * 10 + coords.x].style.background = 'green')
+fetch('bricks.txt').then(response => response.text()).then(bricks => {
+  bricks = bricks.split('\r\n\r\n')
+
+  for (let i = 0; i < bricks.length; i++) {
+    let brick = bricks[i].split('\r\n')
+    brick = brick.map(row => row.match(/../g))
+    const coords = []
+
+    for (let y = 0; y < brick.length; y++) {
+      for (let x = 0; x < brick[y].length; x++) {
+        if (brick[y][x] !== '  ') coords.push({ x, y })
+      }
+    }
+
+    console.log(coords)
+
+    bricks[i] = coords
+
+  }
+  console.log(bricks)
+
+  bricks[rnd(bricks.length)].forEach(coords => block[coords.y * 10 + coords.x].style.background = 'green')
+})
+
+function rnd(max) {
+  return Math.floor(Math.random() * max)
+}
