0e755b96185114c2c08efc861237bc2d47762e8e
daniarkin <jishikukuto@gmail.com>
2022-02-01 13:21:40 +0300

    2 commit

--- js/tetris.js
+++ js/tetris.js
@@ -2,14 +2,33 @@
 
 // const brick = [{ x: 3, y: 3 }, { x: 4, y: 3 }, { x: 5, y: 3 }, { x: 4, y: 4 }];
 const block = document.querySelectorAll('.block');
+const innertState = Array(20).fill().map(() => Array(10).fill(null))
+
+for (let i = 0; i < 6; i++) innertState[19][i] = 'orange'
+for (let i = 6; i < 9; i++) innertState[19][i] = 'green'
+for (let i = 1; i < 5; i++) innertState[18][i] = 'yellow'
+for (let i = 1; i < 5; i++) innertState[17][i] = 'yellow'
+
+console.log(innertState)
+
+
+// const arr = []
+// for (let i=0; i<20; i++) {
+//   const subArr = [];
+//   for (let j = 0; j < 10; j++) {
+//     subArr.push(null)
+//   }
+//   arr.push(subArr)
+// }
 
 fetch('bricks.txt').then(response => response.text()).then(bricks => {
   bricks = bricks.split('\r\n\r\n')
 
   for (let i = 0; i < bricks.length; i++) {
     let brick = bricks[i].split('\r\n')
-    brick = brick.map(row => row.match(/../g))
+    const color = brick.pop()
     const coords = []
+    brick = brick.map(row => row.match(/../g))
 
     for (let y = 0; y < brick.length; y++) {
       for (let x = 0; x < brick[y].length; x++) {
@@ -17,14 +36,14 @@ fetch('bricks.txt').then(response => response.text()).then(bricks => {
       }
     }
 
-    console.log(coords)
-
-    bricks[i] = coords
+    bricks[i] = { coords, color }
 
   }
-  console.log(bricks)
 
-  bricks[rnd(bricks.length)].forEach(coords => block[coords.y * 10 + coords.x].style.background = 'green')
+  const brick = bricks[rnd(bricks.length)]
+  brick.coords.forEach(coords => blocks[coords.y * 10 + coords.x].style.background = brick.color)
+
+
 })
 
 function rnd(max) {
